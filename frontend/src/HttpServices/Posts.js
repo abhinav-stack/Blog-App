eda71226867a97aa8c01f45a46fb5dfb9a33df87
abhinav-stack <72855781+abhinav-stack@users.noreply.github.com>
2022-04-18 17:56:59 +0530

    Add files via upload

---
+++ frontend/src/HttpServices/Posts.js
@@ -0,0 +1,19 @@
+import http from './HttpRequests';
+const baseUrl="http://127.0.0.1:8001";
+
+export const GetAllPosts = ()=> http.get(`${baseUrl}/posts`);
+
+
+export const GetPostDetailsById = ({id}) => http.get(`${baseUrl}/posts/${id}`);
+
+
+export const DeletePostById = ({id}) => http.delete(`${baseUrl}/posts/${id}`);
+
+
+
+export const AddPosts = ({data}) => http.post(`${baseUrl}/posts`,data);
+
+export const UpdatePost = ({id,data})=>http.put(`${baseUrl}/posts/${id}`,data);
+
+
+export const SearchPostData = ({data}) =>http.post(`${baseUrl}/posts/search`,{search:data});
